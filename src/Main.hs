84e7b2f47a5ca033443428a476a76e25504deabf
Greg Hale <imalsogreg@gmail.com>
2015-06-02 22:09:29 -0400

    a little text

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@
 module Main where
 
 import           Data.Fixed           (mod')
+import           Data.Monoid
 import qualified Data.Text            as T
 import           Lucid.Svg
 import qualified Lucid.Svg            as L
@@ -12,9 +13,9 @@ import qualified Lucid.Svg.Attributes as A
 import           Shadow
 
 main :: IO ()
-main = L.renderToFile "test.svg" (svg . dropShadow 2 2 2 . mconcat $ flip map [-4,-3 .. 4] $ \x ->
-                                     mconcat (flip map [-4, -3 .. 4] $ \y ->
-                                       gTranslate (100 * x) (100 * y) (testWedge x y)))
+main = L.renderToFile "test.svg" (svg . (textOnWedge "Hella" [] 100 0 <>) . mconcat $ flip map [-4,-3.5 .. 4] $ \x ->
+                                     mconcat (flip map [-4, -3.5 .. 4] $ \y ->
+                                       gTranslate (150 * x) (150 * y) (testWedge x y)))
 
 testWedge :: Double -> Double -> Svg ()
 testWedge a0 a1 = taurusWedge (TaurusWedgeSpec 0 0 15 35 a0 a1)
@@ -26,7 +27,7 @@ svg content = do
 
 gTranslate :: Double -> Double -> Svg () -> Svg ()
 gTranslate dx dy content = with (g_ content) [transform_ transString]
-  where transString = T.pack . mconcat $ ["translate(", s dx, ",", s dy, ")"]
+  where transString = mconcat $ ["translate(", f dx, ",", f dy, ")"]
 
 data TaurusWedgeSpec = TaurusWedgeSpec {
     tsX  :: Double
@@ -37,26 +38,52 @@ data TaurusWedgeSpec = TaurusWedgeSpec {
   , tsT1 :: Double
   } deriving (Eq, Show)
 
-s :: (RealFrac a, Show a) => a -> String
-s = show . floor
+s :: (RealFrac a, Show a) => a -> T.Text
+s = T.pack . show . floor
 
-sR2 :: (RealFrac a, Show a) => (a,a) -> String
-sR2 (x,y) = s x ++ " " ++ s y
+sR2 :: (RealFrac a, Show a) => (a,a) -> T.Text
+sR2 (x,y) = mconcat [s x, " ", s y]
+
+f :: (RealFrac a, Show a) => a -> T.Text
+f = T.pack . show
+
+fR2 :: (RealFrac a, Show a) => (a,a) -> T.Text
+fR2 (x,y) = mconcat [f x, " ", f y]
+
+circularDist :: Double -> Double -> Double
+circularDist x1 x0 = (x1 - x0 + pi `mod'` (2*pi)) - pi
 
 taurusWedge :: TaurusWedgeSpec -> L.Svg ()
 taurusWedge TaurusWedgeSpec{..} =
-  let p0  = (tsR0 * cos tsT0, tsR1 * sin tsT0)
+  let p0  = (tsR0 * cos tsT0, tsR0 * sin tsT0)
       p1  = (tsR1 * cos tsT0, tsR1 * sin tsT0)
       p2  = (tsR1 * cos tsT1, tsR1 * sin tsT1)
       p3  = (tsR0 * cos tsT1, tsR0 * sin tsT1)
-      largeArc = if (tsT1 - tsT0) `mod'` (2*pi) < pi then "1" else "0"
-      d  = mconcat ["M", s (fst p0), " ", s (snd p0), " "
-                   ,"L ", s (fst p1), " ", s (snd p1)," "
-                   ,"A ", s tsR1, " ", s tsR1, " 0 ", largeArc, " 0 ", s (fst p2), " ", s (snd p2), " "
-                   ,"L ", s (fst p3), " ", s (snd p3)
-                   ,"A ", s tsR0, " ", s tsR0, " 0 ", largeArc, " 1 ", s (fst p0), " ", s (snd p0)
-                   ] :: String
-      -- dHtml = L.toHtml (T.pack d) :: L.Svg ()
+      largeArc = if (tsT1 - tsT0) `mod'` (2*pi) > pi then "1" else "0"
+      d  = mconcat ["M", f (fst p0), " ", f (snd p0), " "
+                   ,"L ", f (fst p1), " ", f (snd p1)," "
+                   ,"A ", f tsR1, " ", f tsR1, " 0 ", largeArc, " 1 ", f (fst p2), " ", f (snd p2), " "
+                   ,"L ", f (fst p3), " ", f (snd p3)
+                   ,"A ", f tsR0, " ", f tsR0, " 0 ", largeArc, " 0 ", f (fst p0), " ", f (snd p0)
+                   ] :: T.Text
       taur0 = do
-        L.path_ [L.d_ (T.pack d)]
+        L.path_ [L.d_ d]
   in taur0
+
+textOnWedge :: T.Text -> [Attribute] -> Double -> Double -> Svg ()
+textOnWedge txt txtAttributes radius theta = g_ $ do
+  defs_ $ do
+    path_ [d_ d, id_ pathName]
+  use_ [xlinkHref_ pathUrl, fill_ "none", stroke_ "red"]
+  text_ [font_family_ "Verdana", font_size_ "30"] $
+    with ((term "textPath" ) (toHtml txt)) [xlinkHref_ pathUrl]
+
+    where
+      d        = mconcat ["M",  f (radius * cos (theta - pi/3)), " "
+                         ,      f (radius * sin (theta - pi/3)), " "
+                         ,"A ", f radius, f radius, " 1 0 0 "
+                         , f (radius * cos (theta + pi/2)), " "
+                         , f (radius * sin (theta + pi/2))
+                         ] :: T.Text
+      pathName = mconcat ["Radius", f radius, "Theta", f theta]
+      pathUrl  = "#" <> pathName
